e9ecf97621029f350dd742bef834689a29c8b291
helpLost <120964309+helpLost@users.noreply.github.com>
2023-06-24 03:06:12 -0400

    quick hotfix

--- files/archive/versions/0.0.150/0.0.110fram/scripts/game/modules/json-data-handler.js
+++
@@ -1,8 +0,0 @@
-import achievementData from './json/achievements.json' assert {type: "json"}
-export const achievements = achievementData;
-
-import researchData from './json/research-trees.json' assert {type: "json"}
-export const researches = researchData;
-
-import defaultStats from './json/civ-stats.json' assert {type: "json"}
-export const stats = defaultStats;
